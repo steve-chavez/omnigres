edcd0656e81b1aed13cf5d01d6a34a78b83a71c3
steve-chavez <stevechavezast@gmail.com>
2024-06-09 11:54:36 -0500

    add nix

---
+++ shell.nix
@@ -0,0 +1,27 @@
+with import (builtins.fetchTarball {
+  name = "2023-09-16";
+  url = "https://github.com/NixOS/nixpkgs/archive/ae5b96f3ab6aabb60809ab78c2c99f8dd51ee678.tar.gz";
+  sha256 = "11fpdcj5xrmmngq0z8gsc3axambqzvyqkfk23jn3qkx9a5x56xxk";
+}) {};
+mkShell {
+  buildInputs = [
+    openssl
+    pkgsStatic.openssl
+    pkg-config
+    cmake
+    flex
+    readline
+    zlib
+    clang-tools
+    python3
+    doxygen
+    libpsl
+    libuv
+    #postgresql_15.withPackages (p: [
+
+    #]);
+ ];
+  shellHook = ''
+    export HISTFILE=.history
+  '';
+}
